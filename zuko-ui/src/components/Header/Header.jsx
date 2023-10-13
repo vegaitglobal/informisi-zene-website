bf235a0e0edc5ade7f1be2a0f64249b2cba98b5b
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-13 21:24:14 +0200

    Create Header component;

---
+++ zuko-ui/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+function Header(){
+    return (
+        <header className={styles.link}>
+            <img src="../logo.png" alt="Logo" />
+            Header            
+        </header>
+    )
+}
+
+export default Header
--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,9 @@
 import TestComponent from "../components/TestComponent/TestComponent";
+import Header from "../components/Header/Header";
 
 export default function HomePage() {
     return <div>
+        <Header />
         <TestComponent testProperty="new text"/>
         <h1>Home Page</h1></div>
 }
