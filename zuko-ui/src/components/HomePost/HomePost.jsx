2889e0c42737e287772a36e94423d12e9c3dd02b
marina.bajic <marina.bajic@vegait.rs>
2023-10-13 23:46:57 +0200

    Initial commit

---
+++ zuko-ui/src/components/HomePost/HomePost.jsx
@@ -0,0 +1,22 @@
+import styles from './HomePost.module.css';
+
+export default function HomePost({ imgSrc = 'www.google.com' }) {
+	return (
+		<div className={styles.post}>
+			<img className={styles.post__img} src={imgSrc} alt="Missing" />
+			<div className={styles.post__content}>
+				<span className={styles.post__subheading}>
+					KOLIKI JE MOJ DEO?
+				</span>
+				<span className={styles.post__heading}>
+					Ruralne Evropljanke
+				</span>
+				<p className={styles.post__desc}>
+					Bits of moving fluff white dwarf kindling the energy hidden
+					in matter shores of the cosmic ocean Jean-François
+					Champollion consciousness.
+				</p>
+			</div>
+		</div>
+	);
+}
--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,12 @@
-import TestComponent from "../components/TestComponent/TestComponent";
+import HomePost from '../components/HomePost/HomePost';
+import TestComponent from '../components/TestComponent/TestComponent';
 
 export default function HomePage() {
-    return <div>
-        <TestComponent testProperty="new text"/>
-        <h1>Home Page</h1></div>
+	return (
+		<div>
+			<TestComponent testProperty="new text" />
+			<h1>Home Page</h1>
+			<HomePost />
+		</div>
+	);
 }
