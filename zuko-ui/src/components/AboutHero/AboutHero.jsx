fee885be6f7678bff8c6723959bda463a2747ca0
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-14 01:07:50 +0200

    About Hero initial commit

--- zuko-ui/src/components/AboutHero/AboutHero.jsx
+++ zuko-ui/src/components/AboutHero/AboutHero.jsx
@@ -1,11 +1,15 @@
-import styles from './AboutHero.module.css'
+import styles from './AboutHero.module.scss'
 
-export default function AboutHero({ header = "O nama", src, alt = "Missing image", text }) {
+export default function AboutHero({ header = "O nama", src, alt = "Missing image", text1, text2, description }) {
     return (
         <div className={styles.container}>
             <h1 className={styles.h1}>{header}</h1>
-            <img className={styles.image} src={src} alt={alt} />
-            <p className={styles.paragraph}>{text}</p>
+            <div className={styles.imgTextComp}>
+                <img className={styles.image} src={src} alt={alt} />
+                <p className={styles.paragraph}>{text1}</p>
+                <p className={styles.paragraph}>{text2}</p>
+                <p className={styles.description}>{description}</p>
+            </div>
         </div>
     );
 }
