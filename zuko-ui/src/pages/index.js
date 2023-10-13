d0aaf915de3f95dce35fdf1f1c48c239ee14d083
marina.bajic <marina.bajic@vegait.rs>
2023-10-14 01:24:42 +0200

    Post share links

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,12 @@
-import TestComponent from "../components/TestComponent/TestComponent";
+import TestComponent from '../components/TestComponent/TestComponent';
+import PostShareLinks from '../components/PostShareLinks/PostShareLinks';
 
 export default function HomePage() {
-    return <div>
-        <TestComponent testProperty="new text"/>
-        <h1>Home Page</h1></div>
+	return (
+		<div>
+			<TestComponent testProperty="new text" />
+			<h1>Home Page</h1>
+			<PostShareLinks />
+		</div>
+	);
 }
