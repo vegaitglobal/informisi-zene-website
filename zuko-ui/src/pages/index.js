ecd9a221f382db43ff0d1b1a8040860cabcabb6d
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-13 22:05:48 +0200

    Rounded button inital commit

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,10 @@
+import RoundedButton from "../components/RoundedButton/RoundedButton";
 import TestComponent from "../components/TestComponent/TestComponent";
 
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="new text"/>
-        <h1>Home Page</h1></div>
+        <h1>Home Page</h1>
+        <RoundedButton />
+        </div>
 }
