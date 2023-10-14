49e5bd5156a3fe24b270806cbe3c9efedfc4e4cb
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-14 10:22:33 +0200

    Finish header

--- zuko-ui/src/components/Header/Header.jsx
+++ zuko-ui/src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 
 function Header(){
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [displayedSubmenu, setDisplayedSubmenu] = useState(null);
-    // const list = useRef(null);
-    // const menu = useRef(null);
 
     const data = [
         {
@@ -60,33 +58,35 @@ function Header(){
 
     return (
         <header className={styles.header}>
-            <img className={styles.logo} src="../logo.png" alt=" Logo" />
-            <div className={styles.holder} style={isMenuOpen ? {transform: `translateY(0px)`} : null}>
-                <ul className={`${styles.nav}`} >
-                    {data?.map((item, index) => (
-                    <li className={styles.item} key={item.label}>
-                        {item?.submenu?.length ? 
-                        <>
-                            <button onClick={() => onClick(index)} className={styles.link} type='button' data-id={index}>{item.label}</button> 
-                            {displayedSubmenu === index && (
-                                <div className={styles.subnav}>
-                                    <ul className={styles.sublist}>
-                                        {item?.submenu.map(subitem => (
-                                            <li className={styles.subitem} key={subitem.label}>
-                                                <a className={styles.sublink} href={subitem.href}>{subitem.label}</a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            )}
-                        </>
-                        : <a href={item.href} className={styles.link}>{item.label}</a>}
-                    </li>))}
-                </ul>
+            <div className={styles.wrap}>
+                <img className={styles.logo} src="../logo.png" alt=" Logo" />
+                <div className={styles.holder} style={isMenuOpen ? {transform: `translateY(0px)`} : null}>
+                    <ul className={`${styles.nav}`} >
+                        {data?.map((item, index) => (
+                        <li className={styles.item} key={item.label}>
+                            {item?.submenu?.length ? 
+                            <>
+                                <button onClick={() => onClick(index)} className={`${styles.link} ${displayedSubmenu === index && styles.linkActive}`} type='button'>{item.label}</button> 
+                                {displayedSubmenu === index && (
+                                    <div className={styles.subnav}>
+                                        <ul className={styles.sublist}>
+                                            {item?.submenu.map(subitem => (
+                                                <li className={styles.subitem} key={subitem.label}>
+                                                    <a className={styles.sublink} href={subitem.href}>{subitem.label}</a>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                )}
+                            </>
+                            : <a href={item.href} className={styles.link}>{item.label}</a>}
+                        </li>))}
+                    </ul>
+                </div>
+                <button className={styles.menuBtn} type='button' onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)}>
+                    <span className={styles.menuIcon}></span>
+                </button>
             </div>
-            <button className={styles.menuBtn} type='button' onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)}>
-                <span className={styles.menuIcon}></span>
-            </button>
         </header>
     )
 }
