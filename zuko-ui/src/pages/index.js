8041fdce710309497d773bc5d16ea221bd7c0708
Petar Purac <petar.purac@vega.local>
2023-10-14 14:50:30 +0200

    finish stats component

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,9 @@
+import Stats from "../components/Stats/Stats";
 import TestComponent from "../components/TestComponent/TestComponent";
 
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="Example component"/>
+        <Stats/>
     </div>
 }
