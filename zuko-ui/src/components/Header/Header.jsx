0a0c937480e62bde9a5b366ab0d8fd5dfcb37cd4
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-14 09:44:16 +0200

    Header changes

--- zuko-ui/src/components/Header/Header.jsx
+++ zuko-ui/src/components/Header/Header.jsx
@@ -1,60 +1,90 @@
-import styles from './Header.module.css';
-
-// const navigation = [
-//     {
-//         name: 'Novosti',
-//         href: 'javascript:;'
-//     },
-//     {
-//         name: 'Publikacije',
-//         href: 'javascript:;'
-//     },
-//     {
-//         name: 'Doniraj',
-//         href: 'javascript:;'
-//     },
-//     {
-//         name: 'O Nama',
-//         href: 'javascript:;'
-//     },
-//     {
-//         name: 'Kontakt',
-//         href: 'javascript:;'
-//     }
-// ]
+import { useLayoutEffect, useRef, useState } from 'react';
+import styles from './Header.module.scss';
 
 function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [displayedSubmenu, setDisplayedSubmenu] = useState(null);
+    // const list = useRef(null);
+    // const menu = useRef(null);
+
+    const data = [
+        {
+            label: 'Novosti',
+            href: '',
+            submenu: [
+                {
+                    label: 'Vest',
+                    href:'www.google.com'
+                },
+                {
+                    label: 'Prilike za žene',
+                    href:'www.google.com'
+                },
+            ]
+        },
+        {
+            label: 'Publikacije',
+            href: '/publications',
+            submenu: [],
+        },
+        {
+            label: 'Doniraj',
+            href: '',
+            submenu: [
+                {
+                    label: 'Vest',
+                    href:'www.google.com'
+                },
+                {
+                    label: 'Prilike za žene',
+                    href:'www.google.com'
+                },
+            ]
+        },
+        {
+            label: 'O Nama',
+            href: '/publications',
+            submenu: [],
+        },
+        {
+            label: 'Kontakt',
+            href: '/publications',
+            submenu: [],
+        }
+    ]
+
+    const onClick = (index) => {
+        if (displayedSubmenu === index) return setDisplayedSubmenu(null);
+        setDisplayedSubmenu(index);
+    };
+
     return (
         <header className={styles.header}>
             <img className={styles.logo} src="../logo.png" alt=" Logo" />
-            <ul className={styles.nav}>
-                <li className={styles.item}>
-                    <button className={styles.link} type='button' href="">Novosti</button>
-                    <div className={styles.subnav}>
-                        <ul className={styles.sublist}>
-                            <li className={styles.subitem}>
-                                <a className={styles.sublink} href="">Vesti</a>
-                            </li>
-                            <li className={styles.subitem}>
-                                <a className={styles.sublink} href="">Prilike za žene</a>
-                            </li> 
-                        </ul>
-                    </div>
-                </li>
-                <li className={styles.item}>
-                    <a className={styles.link} href="">Publikacije</a>
-                </li>
-                <li className={styles.item}>
-                    <a className={styles.link} href="">Doniraj</a>
-                </li>
-                <li className={styles.item}>
-                    <a className={styles.link} href="">O nama</a>
-                </li>
-                <li className={styles.item}>
-                    <a className={styles.link} href="">Kontakt</a>
-                </li>
-            </ul>
-            <button className={styles.menuBtn} type='button'>
+            <div className={styles.holder} style={isMenuOpen ? {transform: `translateY(0px)`} : null}>
+                <ul className={`${styles.nav}`} >
+                    {data?.map((item, index) => (
+                    <li className={styles.item} key={item.label}>
+                        {item?.submenu?.length ? 
+                        <>
+                            <button onClick={() => onClick(index)} className={styles.link} type='button' data-id={index}>{item.label}</button> 
+                            {displayedSubmenu === index && (
+                                <div className={styles.subnav}>
+                                    <ul className={styles.sublist}>
+                                        {item?.submenu.map(subitem => (
+                                            <li className={styles.subitem} key={subitem.label}>
+                                                <a className={styles.sublink} href={subitem.href}>{subitem.label}</a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </>
+                        : <a href={item.href} className={styles.link}>{item.label}</a>}
+                    </li>))}
+                </ul>
+            </div>
+            <button className={styles.menuBtn} type='button' onClick={() => setIsMenuOpen(isMenuOpen => !isMenuOpen)}>
                 <span className={styles.menuIcon}></span>
             </button>
         </header>
