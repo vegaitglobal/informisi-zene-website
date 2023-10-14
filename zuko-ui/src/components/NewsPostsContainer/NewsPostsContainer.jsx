5644d9c89fa56d6d98e84d7df81266907cb0b4d7
marina.bajic <marina.bajic@vegait.rs>
2023-10-14 16:00:15 +0200

    Initial commit

---
+++ zuko-ui/src/components/NewsPostsContainer/NewsPostsContainer.jsx
@@ -0,0 +1,7 @@
+import styles from './NewsPostsContainer.module.scss';
+
+export default function NewsPostsContainer() {
+	return <div>
+
+	</div>;
+}
--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,11 +1,13 @@
 import RoundedButton from "../components/RoundedButton/RoundedButton";
 import TestComponent from "../components/TestComponent/TestComponent";
 import PostShareLinks from '../components/PostShareLinks/PostShareLinks';
+import NewsPostsContainer from "../components/NewsPostsContainer/NewsPostsContainer";
 
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="Example component"/>
         <RoundedButton />
 		<PostShareLinks />
+		<NewsPostsContainer />
     </div>
 }
