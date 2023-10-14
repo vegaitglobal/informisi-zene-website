49b469a9f87d98be00c984a7e11e7fc177b8aecd
Marko Kovacevic <m.kovacevic@vegait.rs>
2023-10-14 17:24:05 +0200

    remove example button from home page

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,4 +1,3 @@
-import RoundedButton from "../components/RoundedButton/RoundedButton";
 import TestComponent from "../components/TestComponent/TestComponent";
 import PostShareLinks from '../components/PostShareLinks/PostShareLinks';
 import HomePostsContainer from "../components/HomePostsContainer/HomePostsContainer";
@@ -6,7 +5,6 @@ import HomePostsContainer from "../components/HomePostsContainer/HomePostsContai
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="Example component"/>
-        <RoundedButton />
 		<HomePostsContainer />
 		<PostShareLinks />
     </div>
