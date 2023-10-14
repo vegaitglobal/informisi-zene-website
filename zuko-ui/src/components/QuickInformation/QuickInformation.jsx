e79af0ece6d4e4867f96a82c00150b7913c063ef
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 00:09:04 +0200

    Fix link

--- zuko-ui/src/components/QuickInformation/QuickInformation.jsx
+++ zuko-ui/src/components/QuickInformation/QuickInformation.jsx
@@ -6,7 +6,7 @@ function QuickInformation(){
         <section className={styles.holder}>
             <img className={styles.image} src='/bulb.svg' alt='Decorative image of bulb' />
             <p className={styles.text}>Imaš ideju kako da unaprediš položaj žena u svojoj zajednici i želiš da se pridružiš ŽUKO timu? </p>
-            <Link to='/' className={styles.link}>Kontaktirajte nas!</Link>
+            <Link to='/contact' className={styles.link}>Kontaktirajte nas!</Link>
         </section>
     )
 }
