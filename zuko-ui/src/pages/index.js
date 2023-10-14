cbc1209c8602e64197f50b43e5ef0701d4f5688c
marina.bajic <marina.bajic@vegait.rs>
2023-10-14 10:50:41 +0200

    Donation hero component

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,9 @@
 import TestComponent from "../components/TestComponent/TestComponent";
+import DonationHero from "../components/DonationHero/DonationHero";
 
 export default function HomePage() {
     return <div>
-        <TestComponent testProperty="Example component"/>
+		<TestComponent testProperty="Example component" />
+		<DonationHero />
     </div>
 }
