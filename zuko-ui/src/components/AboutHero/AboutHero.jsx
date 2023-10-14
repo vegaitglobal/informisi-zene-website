d679fae8e021782b1e8e88ed148a03e7925adc62
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-14 14:09:11 +0200

    Fixed array to const suggestion

--- zuko-ui/src/components/AboutHero/AboutHero.jsx
+++ zuko-ui/src/components/AboutHero/AboutHero.jsx
@@ -1,14 +1,11 @@
 import styles from './AboutHero.module.scss'
 
-export default function AboutHero({ header = "O nama", src, alt = "Missing image", text1, text2, description }) {
-    const texts = [
-        {
-            'text1' : 'Žensko udruženje kolubarskog okruga (ŽUKO) je osnovano u maju 2013. godine sa idejom unapređenja položaja žena na lokalnom nivou.',
-            'text2' : 'Vremenom su se, sa rastom udruženja, i ciljevi rada jasnije definisali, a danas ŽUKO aktivno deluje širom Srbije.',
-            'description' : 'Zahvaljujući podršci pojedinaca, organizacija, fondacija, kompanija i drugih saradnika, ŽUKO je uspešno sproveo preko 30 projekata koji su značajno doprineli boljem životu žena, posebno žena na selu i njihovih porodica.',
-            'src' : './AboutHero.png'
-        }
-    ]
+export default function AboutHero({ header = "O nama", src = srcDefault, alt = "Missing image", text1 = text1Default, text2 = text2Default, description = descriptionDefault }) {
+    const text1Default = 'Žensko udruženje kolubarskog okruga (ŽUKO) je osnovano u maju 2013. godine sa idejom unapređenja položaja žena na lokalnom nivou.',
+    const text2Default =  'Vremenom su se, sa rastom udruženja, i ciljevi rada jasnije definisali, a danas ŽUKO aktivno deluje širom Srbije.',
+    const descriptionDefault =  'Zahvaljujući podršci pojedinaca, organizacija, fondacija, kompanija i drugih saradnika, ŽUKO je uspešno sproveo preko 30 projekata koji su značajno doprineli boljem životu žena, posebno žena na selu i njihovih porodica.',
+    const srcDefault = './AboutHero.png'
+
     return (
         <div className={styles.container}>
             <h1 className={styles.h1}>{header}</h1>
--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import AboutHero from "../components/AboutHero/AboutHero";
-=======
 import RoundedButton from "../components/RoundedButton/RoundedButton";
->>>>>>> develop
 import TestComponent from "../components/TestComponent/TestComponent";
 
 export default function HomePage() {
     return <div>
        
         <TestComponent testProperty="Example component"/>
-<<<<<<< HEAD
         <AboutHero />
-=======
         <RoundedButton />
->>>>>>> develop
     </div>
 }
