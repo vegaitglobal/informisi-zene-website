08e0e38f6a6908cfc5c005fee199db4dba1e36f9
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-14 20:02:05 +0200

    Finish horizontal spacer

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -3,12 +3,14 @@ import PostShareLinks from '../components/PostShareLinks/PostShareLinks';
 import HomePostsContainer from "../components/HomePostsContainer/HomePostsContainer";
 import StrategyGoals from "../components/StrategyGoals/StrategyGoals";
 import HeroBanner from '../components/HeroBanner/HeroBanner';
+import HorizontalSpacer from "../components/HorizontalSpacer/HorizontalSpacer";
 
 export default function HomePage() {
     return <div>
         <HeroBanner />
         <StrategyGoals />
 		<HomePostsContainer />
+        <HorizontalSpacer desktopSize={180} mobileSize={90} />
         <Stats/>
 		<PostShareLinks />
     </div>
