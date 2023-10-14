2771ab347f10bf869406d7c49d01fd1e8b88a37e
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-14 10:58:05 +0200

    fe/Added basic search div and css

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,7 +1,10 @@
 import TestComponent from "../components/TestComponent/TestComponent";
+import SearchPostInput from "../components/SearchPostsInput/SearchPostInput";
 
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="new text"/>
-        <h1>Home Page</h1></div>
+        <h1>Home Page</h1>
+        <SearchPostInput/>
+        </div>
 }
