c2efb58705b0e82a7808059910a95ec51de947ca
marina.bajic <marina.bajic@vegait.rs>
2023-10-14 15:30:57 +0200

    Added image to public folder and refactored

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,11 +1,11 @@
 import TestComponent from '../components/TestComponent/TestComponent';
-import HomePostsContainer from '../components/HomePostsContainer/HomePostsContainer';
+import HomePost from '../components/HomePost/HomePost';
 
 export default function HomePage() {
 	return (
 		<div>
 			<TestComponent testProperty="Example component" />
-			<HomePostsContainer />
+			<HomePost />
 		</div>
 	);
 }
