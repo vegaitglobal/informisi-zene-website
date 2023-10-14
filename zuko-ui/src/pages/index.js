e90e8c7d3fdf265729a6a4a54f44c3b6196bfcee
Marko Kovacevic <m.kovacevic@vegait.rs>
2023-10-14 16:41:45 +0200

    Upload home post container

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,13 +1,13 @@
 import RoundedButton from "../components/RoundedButton/RoundedButton";
-import HomePost from '../components/HomePost/HomePost';
 import TestComponent from "../components/TestComponent/TestComponent";
 import PostShareLinks from '../components/PostShareLinks/PostShareLinks';
+import HomePostsContainer from "../components/HomePostsContainer/HomePostsContainer";
 
 export default function HomePage() {
     return <div>
         <TestComponent testProperty="Example component"/>
         <RoundedButton />
-		<HomePost />
+		<HomePostsContainer />
 		<PostShareLinks />
     </div>
 }
