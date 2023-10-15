fb95355dfd87a2045cc186c4caee3bcadea68393
unknown <bulatovic.vlastimir98@gmail.com>
2023-10-15 17:35:28 +0200

    Added container wraper for Post article and post aside

--- zuko-ui/src/components/PostContainer/PostContainer.jsx
+++ zuko-ui/src/components/PostContainer/PostContainer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Article from '../Article/Article'
 import style from "./PostContainer.module.scss";
+import BlogRecommendationContainer from './../BlogRecommendationContainer/BlogRecommendationContainer';
 
-export default function PostContainer({data}) {
+export default function PostContainer({data, blogs}) {
   return (
     <section className={style.postContainer}>
         <div className={style.article}>
             <Article data={data}/>
         </div>
         <div className={style.aside}>
-
+          <BlogRecommendationContainer blogs={blogs}/>
         </div>
     </section>
   )
