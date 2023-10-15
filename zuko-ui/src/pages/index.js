bcf9cced755341a5ef57f54b8228ddf1bb2888cc
marina.bajic <marina.bajic@vegait.rs>
2023-10-15 14:09:41 +0200

    Added navigation to opportunities page

--- zuko-ui/src/pages/index.js
+++ zuko-ui/src/pages/index.js
@@ -1,25 +1,39 @@
-import { useEffect, useState } from "react";
-import Stats from "../components/Stats/Stats";
-import QuickInformation from "../components/QuickInformation/QuickInformation";
-import HomePostsContainer from "../components/HomePostsContainer/HomePostsContainer";
-import StrategyGoals from "../components/StrategyGoals/StrategyGoals";
+import { useEffect, useState } from 'react';
+import Stats from '../components/Stats/Stats';
+import QuickInformation from '../components/QuickInformation/QuickInformation';
+import HomePostsContainer from '../components/HomePostsContainer/HomePostsContainer';
+import StrategyGoals from '../components/StrategyGoals/StrategyGoals';
 import HeroBanner from '../components/HeroBanner/HeroBanner';
-import { getNumberOfPostsService } from "../services/posts.service";
+import { getNumberOfPostsService } from '../services/posts.service';
+import RoundedButton from '../components/RoundedButton/RoundedButton';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
-    const [listOfPosts, setListOfPosts] = useState([]);
+	const [listOfPosts, setListOfPosts] = useState([]);
 
-    useEffect(()=>{
-        getNumberOfPostsService(2)
-            .then(setListOfPosts)
-            .catch(error => console.error(error));
-    },[])
+	useEffect(() => {
+		getNumberOfPostsService(2)
+			.then(setListOfPosts)
+			.catch((error) => console.error(error));
+	}, []);
 
-    return <div>
-        <HeroBanner />
-        <StrategyGoals />
-		<HomePostsContainer data={listOfPosts} />
-        <Stats/>
-        <QuickInformation />
-    </div>
+	let navigate = useNavigate();
+	const goToOpportunities = () => {
+		let path = `/opportunities`;
+		navigate(path);
+	};
+
+	return (
+		<div>
+			<HeroBanner />
+			<StrategyGoals />
+			<HomePostsContainer data={listOfPosts} />
+			<RoundedButton
+				label="Sve prilike za žene"
+				onClick={goToOpportunities}
+			/>
+			<Stats />
+			<QuickInformation />
+		</div>
+	);
 }
