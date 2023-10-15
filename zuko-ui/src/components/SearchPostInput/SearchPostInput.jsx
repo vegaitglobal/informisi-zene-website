ff7bf9071ec3d871bca0911241799cf9dd00cc0e
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 15:23:48 +0200

    Add debounce on search

--- zuko-ui/src/components/SearchPostInput/SearchPostInput.jsx
+++ zuko-ui/src/components/SearchPostInput/SearchPostInput.jsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from 'react'
 import styles from './SearchPostInput.module.scss'
 
 export default function SearchPostInput({label = "Pretraga", setQuery}){
+
+    const handleSearch = (e) => {
+        const getData = setTimeout(() => {
+            setQuery(e.target.value);
+        }, 300)
+
+        return () => clearTimeout(getData);
+    }
     
     return <div>
         <p className={styles.title}>{label}</p>
-        <input className={styles.search} placeholder={label} type="text" onChange={(e) => setQuery(e.target.value)}/>
+        <input className={styles.search} placeholder={label} type="text" onChange={handleSearch}/>
     </div>
 }
