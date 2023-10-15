9fa41efcf0a49b130a0dba26c7010193ce7a694e
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 16:57:15 +0200

    Fix news layout styles

--- zuko-ui/src/pages/news.page.js
+++ zuko-ui/src/pages/news.page.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { getByQueryService } from '../services/posts.service';
 import PostsContainer from '../components/PostsContainer/PostsContainer';
 import SearchPostInput from '../components/SearchPostInput/SearchPostInput';
+import GeneralHeading from '../components/GeneralHeading/GeneralHeading';
+import HorizontalSpacer from '../components/HorizontalSpacer/HorizontalSpacer';
 
 export default function NewsPage() {
 	const [query, setQuery] = useState('');
@@ -15,6 +17,7 @@ export default function NewsPage() {
 		<div>
 			<SearchPostInput setQuery={setQuery} />
 			<PostsContainer displayVersion="v2" data={searchedData.data} />
+			<HorizontalSpacer desktopSize={120} mobileSize={60} />
 		</div>
 	);
 }
