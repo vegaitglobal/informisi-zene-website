8b67cb97ce913a730a03bf1b82eedccc87c318b7
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 11:43:28 +0200

    Fix console key warning

--- zuko-ui/src/components/Article/Article.jsx
+++ zuko-ui/src/components/Article/Article.jsx
@@ -1,6 +1,7 @@
 import styles from './Article.module.scss';
 import PostShareLinks from '../PostShareLinks/PostShareLinks';
 import parse from 'html-react-parser';
+import { Fragment } from 'react';
 
 function Article({data = {}}){
     const blocks = data.blocks;
@@ -8,14 +9,14 @@ function Article({data = {}}){
         <article className={styles.article}>
             <span className={styles.subtitle}>Koliki je moj deo?</span>
             {data?.title ? <h2 className={styles.title}>{data?.title}</h2> : null}
-            {blocks?.map((el)=>{
+            {blocks?.map((el, i)=>{
                 return (
-                    <>
+                    <Fragment key={i}>
                         {el.type === 'text' && <div className={styles.rte}>
                             {parse(el?.value)}
                         </div>}
                         {el.type === 'image' && <img className={styles.image} src={el?.value} alt="Post image" />}
-                    </>
+                    </Fragment>
                 )
             })}
             <PostShareLinks />
