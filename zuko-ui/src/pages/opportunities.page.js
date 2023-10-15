ca23957f1b69c0f5bff800d6687cf736197464e3
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-15 15:22:20 +0200

    added service for categories

--- zuko-ui/src/pages/opportunities.page.js
+++ zuko-ui/src/pages/opportunities.page.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
+import {getCategoriesService} from '../services/categories.service';
+import { getPostsByCategory } from "../services/posts.service";
+
 export default function OpportunitiesPage() {
     const [listOfCategories, setListOfCategories] = useState([]);
-
+    const [listOfPosts, setListOfPosts] = useState([]);
     useEffect(()=>{
         getCategoriesService()
             .then(setListOfCategories)
             .catch(error => console.error(error));
     },[])
-    
+    /* 
+        This is how to get post by category when selected
+        getPostsByCategory(categoryId)
+            .then(setListOfPosts)
+            .catch(error => console.error(error));
+    */
     return <div>
         Prilike za zene
     </div>
