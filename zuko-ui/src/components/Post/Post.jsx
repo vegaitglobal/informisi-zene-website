12f144f6a05a2d173226da0e6fde810730ac87d3
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 15:51:52 +0200

    Fix Post and Postcontainer components

--- zuko-ui/src/components/Post/Post.jsx
+++ zuko-ui/src/components/Post/Post.jsx
@@ -3,37 +3,35 @@ import styles from './Post.module.scss';
 
 export default function Post({ postData = {} }) {
 	return (
-		<div>
-			<div className={styles.post}>
-				<NavLink to={`posts/${postData.id}`} target="_blank">
-					<div className={styles.post__img}>
-						<img
-							src={postData?.cover_image}
-							alt={postData.title}
-						/>
-					</div>
-				</NavLink>
-				<div className={styles.post__content}>
-					{postData.categories && (
-						postData.categories.map((category) => (
-							<span key={category.id} className={styles.post__subheading}>
-								{category.name}
-							</span>
-						))
-					)}
-					<NavLink to={`posts/${postData.id}`} target="_blank">
-						{postData.title && (
-							<span className={styles.post__heading}>
-								{postData.title}
-							</span>
-						)}
-					</NavLink>
-					{postData.description && (
-						<p className={styles.post__desc}>
-							{postData.description}
-						</p>
-					)}
+		<div className={styles.post}>
+			<NavLink to={`posts/${postData.id}`} target="_blank">
+				<div className={styles.post__img}>
+					<img
+						src={postData?.cover_image}
+						alt={postData.title}
+					/>
 				</div>
+			</NavLink>
+			<div className={styles.post__content}>
+				{postData.categories && (
+					postData.categories.map((category) => (
+						<span key={category.id} className={styles.post__subheading}>
+							{category.name}
+						</span>
+					))
+				)}
+				<NavLink to={`posts/${postData.id}`} target="_blank">
+					{postData.title && (
+						<span className={styles.post__heading}>
+							{postData.title}
+						</span>
+					)}
+				</NavLink>
+				{postData.description && (
+					<p className={styles.post__desc}>
+						{postData.description}
+					</p>
+				)}
 			</div>
 		</div>
 	);
