c5d5b1c0a5daf0de598e41a41ca5cfbeb3530ff9
unknown <bulatovic.vlastimir98@gmail.com>
2023-10-15 15:08:46 +0200

    Added heading component on Opportunities and Publication pages

--- zuko-ui/src/components/PublicationHero/PublicationHero.jsx
+++ zuko-ui/src/components/PublicationHero/PublicationHero.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import style from "./OpportunitiesHero.module.scss";
+import style from "./PublicationHero.module.scss";
 import CategoriesSelect from "../CategoriesSelect/CategoriesSelect";
 import GeneralHeading from './../GeneralHeading/GeneralHeading';
 
-export default function OpportunitiesHero({list = [], onSelectChange = () => {} }) {
-
-
+export default function PublicationHero({list = [], onSelectChange = () => {} }) {
     return (
         <div className={style.hero}>
-            <GeneralHeading label='Prilike za žene'/>
+            <GeneralHeading label='Publikacije'/>
 
             <h3 className={style.subtitle}>
-                Sortiraj po projektu
+                Sortiraj po vrsti
             </h3>
             <div className={style.categoriesContainer}>
-                <CategoriesSelect options={list} defaultSelection="Svi projekti" onSelectChange={onSelectChange} />
+                <CategoriesSelect options={list} defaultSelection="Sve vrste" onSelectChange={onSelectChange} />
             </div>
-            
         </div>
     );
 }
--- zuko-ui/src/components/SearchPostInput/SearchPostInput.jsx
+++ zuko-ui/src/components/SearchPostInput/SearchPostInput.jsx
@@ -4,5 +4,5 @@ export default function SearchPostInput({label = "Pretraga"}){
     return <div>
         <p className={styles.title}>{label}</p>
         <input className={styles.search} placeholder={label} type="text"/>
-        </div>
+    </div>
 }
