88475351bd0cfed7fbfaee2a7f9837608c81bb2c
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-15 14:43:39 +0200

    added service for categories

--- zuko-ui/src/pages/opportunities.page.js
+++ zuko-ui/src/pages/opportunities.page.js
@@ -1,4 +1,12 @@
 export default function OpportunitiesPage() {
+    const [listOfCategories, setListOfCategories] = useState([]);
+
+    useEffect(()=>{
+        getCategoriesService()
+            .then(setListOfCategories)
+            .catch(error => console.error(error));
+    },[])
+    
     return <div>
         Prilike za zene
     </div>
