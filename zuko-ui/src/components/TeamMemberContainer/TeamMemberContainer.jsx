ab3cfe7b4ab185c30b433dca820422b5495d20d1
marina.bajic <marina.bajic@vegait.rs>
2023-10-15 17:14:11 +0200

    Fixed images

--- zuko-ui/src/components/TeamMemberContainer/TeamMemberContainer.jsx
+++ zuko-ui/src/components/TeamMemberContainer/TeamMemberContainer.jsx
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from "react";
-import styles from "./TeamMemberContainer.module.scss";
-import TeamMember from "../TeamMember/TeamMember";
-import { getEmployeesService } from "../../services/employees.service";
+import React, { useState, useEffect } from 'react';
+import styles from './TeamMemberContainer.module.scss';
+import TeamMember from '../TeamMember/TeamMember';
+import { getEmployeesService } from '../../services/employees.service';
 
-export default  function TeamMemberContainer() {
-    const [team, setTeam] = useState([]);
+export default function TeamMemberContainer() {
+	const [team, setTeam] = useState([]);
 
-    const mockData = {
-        title: "Naš tim",
-        data: [
-            {
-                imageLink: "https://placehold.co/260x315/webp",
-                name: "Jelena Ružić",
-            },
-            {
-                imageLink: "https://placehold.co/260x315/webp",
-                name: "Jelena Ružić",
-            },
-            {
-                imageLink: "https://placehold.co/260x315/webp",
-                name: "Jelena Ružić",
-            },
-            {
-                imageLink: "https://placehold.co/260x315/webp",
-                name: "Jelena Ružić",
-            }
-        ]
-    }
+	const mockData = {
+		title: 'Naš tim',
+		data: [
+			{
+				profileImageUrl: 'https://placehold.co/260x315/webp',
+				name: 'Jelena Ružić',
+			},
+			{
+				profileImageUrl: 'https://placehold.co/260x315/webp',
+				name: 'Jelena Ružić',
+			},
+			{
+				profileImageUrl: 'https://placehold.co/260x315/webp',
+				name: 'Jelena Ružić',
+			},
+			{
+				profileImageUrl: 'https://placehold.co/260x315/webp',
+				name: 'Jelena Ružić',
+			},
+		],
+	};
 
-    useEffect(() => {
+	useEffect(() => {
 		getEmployeesService()
 			.then(setTeam)
 			.catch((error) => console.error(error));
 	}, []);
-console.log(team);
-    return (
+	console.log(team);
+	return (
 		<section className={styles.team}>
 			<h2 className={styles.title}>Naš tim</h2>
 			<div className={styles.teamContainer}>
 				{team &&
-					team.map(({ imageLink, name }, index) => (
+					team.map(({ name, profileImageUrl }, index) => (
 						<TeamMember
-							imageLink={imageLink}
 							name={name}
+							profileImageUrl={profileImageUrl}
 							key={`team-member-${index}`}
 						/>
 					))}
