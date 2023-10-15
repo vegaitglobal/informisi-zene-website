6089365af4de794673e63a7d901b22927b3ff886
unknown <bulatovic.vlastimir98@gmail.com>
2023-10-15 11:15:42 +0200

    Integrated with post service

--- zuko-ui/src/components/BlogRecommendationContainer/BlogRecommendationContainer.jsx
+++ zuko-ui/src/components/BlogRecommendationContainer/BlogRecommendationContainer.jsx
@@ -1,32 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import style from "./BlogRecommendationContainer.module.scss";
 import BlogRecommendation from '../BlogRecommendation/BlogRecommendation';
 
-export default function BlogRecommendationContainer() {
-	const [blogs, setBlogs] = useState([]);
-
-	const mockBlogs = [
-        {
-            "id": 1,
-            "title": "Ruralne Evropljanke",
-            "user": "Jelena",
-            "created":"2023-10-13T18:00",
-            "description" : "Bits of moving fluff white dwarf kindling the energy hidden in matter shores of the cosmic ocean Jean-François Champollion consciousness."
-        },
-		{
-            "id": 2,
-            "title": "(Ne)pravedno nasleđivanje, Insajder TV",
-            "user": "Jelena",
-            "created":"2023-10-13T18:00",
-            "description" : "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
-        }
-    ];
-
-	useEffect(() => {
-		setBlogs(mockBlogs);
-	}, [])
-	
-
+export default function BlogRecommendationContainer({blogs}) {
+	console.log(blogs)
 	return (
 		<aside className={style.blogRecommendationContainer}>
 			<h2 className={style.title}>
@@ -34,7 +11,7 @@ export default function BlogRecommendationContainer() {
 			</h2>
 
 			<div className={style.blogs}>
-				{blogs.map(({id, title, description}) => (
+				{blogs.length && blogs.map(({id, title, description}) => (
 					<BlogRecommendation 
 						title={title}
 						description={description}
--- zuko-ui/src/pages/post.page.js
+++ zuko-ui/src/pages/post.page.js
@@ -1,7 +1,18 @@
+import React, {useEffect, useState} from "react";
 import Article from '../components/Article/Article'
+import { getNumberOfPostsService } from "../services/posts.service";
+import BlogRecommendationContainer from './../components/BlogRecommendationContainer/BlogRecommendationContainer';
 
 export default function PostPage() {
+    const [recommendedPosts, setRecommendedPosts] = useState([]);
+
+    useEffect(()=>{
+        getNumberOfPostsService(2)
+            .then(setRecommendedPosts);
+    },[])
+
     return <div>
         <Article />
+        <BlogRecommendationContainer blogs={recommendedPosts}/>
     </div>
 }
