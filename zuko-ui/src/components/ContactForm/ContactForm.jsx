7628b4d389560c3c841db83a5199107d688a4ff3
OgnjenCankovic <cankovicognjen@gmail.com>
2023-10-15 09:36:01 +0200

    Contact us page implemented

--- zuko-ui/src/components/ContactForm/ContactForm.jsx
+++ zuko-ui/src/components/ContactForm/ContactForm.jsx
@@ -30,55 +30,55 @@ export default function ContactForm({ heading = "Pišite nam" }) {
         <form className={styles.wrapper} onSubmit={handleSubmit}>
 
 
-                <input
-                    className={styles.input}
-                    type="text"
-                    name="ime"
-                    placeholder='Ime'
-                    value={formData.ime}
-                    onChange={handleInputChange}
-                    required
-                />
+            <input
+                className={styles.input}
+                type="text"
+                name="ime"
+                placeholder='Ime'
+                value={formData.ime}
+                onChange={handleInputChange}
+                required
+            />
 
 
-                <input
-                    className={styles.input}
-                    type="email"
-                    name="mail"
-                    placeholder='Mail'
-                    value={formData.mail}
-                    onChange={handleInputChange}
-                    required
-                />
+            <input
+                className={styles.input}
+                type="email"
+                name="mail"
+                placeholder='Mail'
+                value={formData.mail}
+                onChange={handleInputChange}
+                required
+            />
 
-                <input
-                    className={styles.input}
-                    type="tel"
-                    name="telefon"
-                    placeholder='Telefon'
-                    value={formData.telefon}
-                    onChange={handleInputChange}
-                    required
-                />
-                <input
-                    className={styles.input}
-                    type="text"
-                    name="naslov"
-                    placeholder='Naslov'
-                    value={formData.naslov}
-                    onChange={handleInputChange}
-                    required
-                />
-                <textarea
-                    className={styles.textarea}
-                    name="poruka"
-                    placeholder='Poruka'
-                    value={formData.poruka}
-                    onChange={handleInputChange}
-                    required
-                />
-                <div className={styles.button}>
-            <RoundedButton buttonType='submit' label='Pošalji'/>
+            <input
+                className={styles.input}
+                type="tel"
+                name="telefon"
+                placeholder='Telefon'
+                value={formData.telefon}
+                onChange={handleInputChange}
+                required
+            />
+            <input
+                className={styles.input}
+                type="text"
+                name="naslov"
+                placeholder='Naslov'
+                value={formData.naslov}
+                onChange={handleInputChange}
+                required
+            />
+            <textarea
+                className={styles.textarea}
+                name="poruka"
+                placeholder='Poruka'
+                value={formData.poruka}
+                onChange={handleInputChange}
+                required
+            />
+            <div className={styles.button}>
+                <RoundedButton buttonType='submit' label='Pošalji' />
             </div>
         </form>
     </div>
