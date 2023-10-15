e53d418f55b248fa946b18a372435884d43462d6
Petar Purac <petar.purac@vega.local>
2023-10-15 17:17:32 +0200

    Fix pagination on the news page

--- zuko-ui/src/pages/news.page.js
+++ zuko-ui/src/pages/news.page.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { getByQueryService } from '../services/posts.service';
+import { getByQueryService, getPostsService } from '../services/posts.service';
 import PostsContainer from '../components/PostsContainer/PostsContainer';
 import SearchPostInput from '../components/SearchPostInput/SearchPostInput';
+import Pagination from '../components/Pagination/Pagination';
 
 export default function NewsPage() {
 	const [query, setQuery] = useState('');
-	const [searchedData, setSearchedData] = useState([]);
     const [data, setData] = useState([])
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState();
@@ -16,24 +16,32 @@ export default function NewsPage() {
     }
 
     useEffect(() => {
-        getByQueryService({query}).then(setSearchedData);
+        getByQueryService({query}).then(response => {
+            setTotalPages(response.meta.last_page);
+            setData(response)
+        });
 	}, [query]);
 
     useEffect(()=>{
-        getPostsService(currentPage).then(response => {
-            const responseData = response.data;
-            setTotalPages(response.meta.last_page)
-            setData(prev => ([...prev, ...responseData]))
+        if(!data?.data) return; 
+        
+        getByQueryService({query, pageNumber: currentPage}).then(response => {
+            const responseData = response;
+            setTotalPages(responseData.meta.last_page);
+            setData(prev => {
+                return {
+                    ...prev,
+                    data: [...prev.data, ...responseData.data]
+                }
+            });
         })
     },[currentPage]);
 
-    console.log(data)
 
 	return (
 		<div>
 			<SearchPostInput setQuery={setQuery} />
-            {data.map(item => (<div>{item.title}</div>))}
-			<PostsContainer displayVersion="v2" data={searchedData.data} />
+			<PostsContainer displayVersion="v2" data={data.data} />
             {currentPage === totalPages ? <></> : <Pagination onClick={handlePaginationClick}/> }
 		</div>
 	);
--- zuko-ui/src/services/posts.service.js
+++ zuko-ui/src/services/posts.service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function getPostsService(pageNumber = 1) {
     const response = await axios
-        .get(`${process.env.REACT_APP_ENDPOINT}posts?page=${pageNumber}`)
+        .get(`${process.env.REACT_APP_ENDPOINT}posts?page=${pageNumber}&size=6`)
         .catch(error => console.error(error));
 
     return response?.data;
@@ -16,9 +16,9 @@ export async function getPostService({id}) {
     return response.data;
 }
 
-export async function getByQueryService({query = ''}) {
+export async function getByQueryService({query = '', pageNumber = 1}) {
     const response = await axios
-        .get(`${process.env.REACT_APP_ENDPOINT}posts?title[like]=${query}`)
+        .get(`${process.env.REACT_APP_ENDPOINT}posts?title[like]=${query}&page=${pageNumber}&size=6`)
         .catch(error => console.error(error));
 
     return response.data;
