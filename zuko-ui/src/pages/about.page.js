6e47a07acad8fe76dc3c637d290582b880683cc4
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-10-15 17:06:26 +0200

    Fix minor style changes

--- zuko-ui/src/pages/about.page.js
+++ zuko-ui/src/pages/about.page.js
@@ -1,11 +1,13 @@
 import AboutHero from '../components/AboutHero/AboutHero';
 import AnnualReport from '../components/AnnualReport/AnnualReport';
+import HorizontalSpacer from '../components/HorizontalSpacer/HorizontalSpacer';
 import TeamMemberContainer from "../components/TeamMemberContainer/TeamMemberContainer";
 import DonorsContainer from './../components/DonorsContainer/DonorsContainer';
 import OurValues from './../components/OurValues/OurValues';
 
 export default function AboutPage() {
     return <div>
+        <HorizontalSpacer desktopSize={90} mobileSize={0} />
         <AboutHero />
         <OurValues/>
         <TeamMemberContainer/>
