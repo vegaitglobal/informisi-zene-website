3a499146f267eb0e9c4ab7fdd42289da520f50c5
markopuzovic <147758140+markopuzovic@users.noreply.github.com>
2023-10-17 14:10:12 +0200

    fix qr code (#69)

--- zuko-ui/src/components/QRCodeItem/QRCodeItem.jsx
+++ zuko-ui/src/components/QRCodeItem/QRCodeItem.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import styles from './QRCodeItem.module.scss'
+import React from "react";
+import styles from "./QRCodeItem.module.scss";
 
-const QRCodeItem = ({QRUrl}) => {
+const QRCodeItem = ({ QRUrl }) => {
   return (
-		<div className={styles.QRCodeItem}>
-			<h3 className={styles.QRCodeTitle}>QR kod za donacije</h3>
-			<div className={styles.QRCode}>
-				<img
-					src="https://cdn.britannica.com/17/155017-050-9AC96FC8/Example-QR-code.jpg"
-					alt="ZUKO QR code"
-					className={styles.QRCodeImg}
-				/>
-			</div>
-		</div>
+    <div className={styles.QRCodeItem}>
+      <h3 className={styles.QRCodeTitle}>QR kod za donacije</h3>
+      <div className={styles.QRCode}>
+        <img
+          src={`data:image/jpeg;base64,${QRUrl}`}
+          alt="ZUKO QR code"
+          className={styles.QRCodeImg}
+        />
+      </div>
+    </div>
   );
-}
+};
 
-export default QRCodeItem
+export default QRCodeItem;
--- zuko-ui/src/services/donationService.js
+++ zuko-ui/src/services/donationService.js
@@ -1,32 +1,36 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getDonationInfo = async () => {
-    try {
-        const res = await axios.get('https://144251f0-e5d6-4abf-90cc-a4878ba392b0.mock.pstmn.io/api/1/donation-infos');
-        const donationData = res.data;
-        
-        const [data] = donationData;
-        console.log(data)
-        const {account_number, recepient_name, default_amount, payment_code} = data;
-        // R: should be changed dynamically
-        const qrImage = await axios.post('https://www.nbs.rs/QRcode/api/qr/v1/generate?lang=en',`K:PR|V:01|C:1|R:160000000039175078|N:${recepient_name}|I:RSD2424,00|SF:${payment_code}`,
-          {
-            headers: {
-              'Content-Type': 'text/plain'
-            }
-          }
-        );
-        console.log(qrImage)
-        return {donationData, qrImage}
-      } catch (error) {
-        console.log(error);
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_ENDPOINT}donations/donation-info`
+    );
+    const donationData = res.data;
+
+    const [data] = donationData;
+    console.log(data);
+    const { account_number, recepient_name, default_amount, payment_code } =
+      data;
+    // R: should be changed dynamically
+    const qrImage = await axios.post(
+      "https://www.nbs.rs/QRcode/api/qr/v1/generate?lang=en",
+      `K:PR|V:01|C:1|R:${account_number}|N:${recepient_name}|I:RSD${default_amount},00|SF:${payment_code}`,
+      {
+        headers: {
+          "Content-Type": "text/plain",
+        },
       }
-}
+    );
+    return { donationData, qrImage };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export async function getDonationSites() {
-  const response= await axios
+  const response = await axios
     .get(`${process.env.REACT_APP_ENDPOINT}donations/donors`)
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 
-    return response.data
-;}
+  return response.data;
+}
