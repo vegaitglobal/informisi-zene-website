cd5897b32a15de54126c22b6269bb0ec7328aa49
markopuzovic <m.puzovic@vegait.rs>
2023-10-23 16:09:55 +0200

    add run migration in composer and remove the migration endpoint

--- zuko-api/routes/web.php
+++ zuko-api/routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/main');
 });
-
-Route::get('/migrate', function () {
-    \Artisan::call('migrate');
-    return \Artisan::output();
-});
