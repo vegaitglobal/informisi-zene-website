1e94db129339fa76e6756b07ef033d818afa9fee
markopuzovic <m.puzovic@vegait.rs>
2023-10-23 15:24:07 +0200

    quick fix to migrate with endpoint

--- zuko-api/routes/web.php
+++ zuko-api/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/main');
 });
+
+Route::get('/migrate', function () {
+    \Artisan::call('migrate');
+    return \Artisan::output();
+});
