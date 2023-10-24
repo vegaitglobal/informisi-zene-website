fdf1e20948b8d29d091804ad59ca81ba134a0608
markopuzovic <m.puzovic@vegait.rs>
2023-10-24 16:52:30 +0200

    show post page and change route

--- zuko-ui/src/components/Post/Post.jsx
+++ zuko-ui/src/components/Post/Post.jsx
@@ -1,38 +1,30 @@
-import { NavLink } from 'react-router-dom';
-import styles from './Post.module.scss';
+import { NavLink } from "react-router-dom";
+import styles from "./Post.module.scss";
 
 export default function Post({ postData = {} }) {
-	return (
-		<div className={styles.post}>
-			<NavLink to={`posts/${postData.id}`} target="_blank">
-				<div className={styles.post__img}>
-					<img
-						src={postData?.cover_image}
-						alt={postData.title}
-					/>
-				</div>
-			</NavLink>
-			<div className={styles.post__content}>
-				{postData.categories && (
-					postData.categories.map((category) => (
-						<span key={category.id} className={styles.post__subheading}>
-							{category.name}
-						</span>
-					))
-				)}
-				<NavLink to={`posts/${postData.id}`} target="_blank">
-					{postData.title && (
-						<span className={styles.post__heading}>
-							{postData.title}
-						</span>
-					)}
-				</NavLink>
-				{postData.description && (
-					<p className={styles.post__desc}>
-						{postData.description}
-					</p>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.post}>
+      <NavLink to={`/posts/${postData.id}`} target="_blank">
+        <div className={styles.post__img}>
+          <img src={postData?.cover_image} alt={postData.title} />
+        </div>
+      </NavLink>
+      <div className={styles.post__content}>
+        {postData.categories &&
+          postData.categories.map((category) => (
+            <span key={category.id} className={styles.post__subheading}>
+              {category.name}
+            </span>
+          ))}
+        <NavLink to={`/posts/${postData.id}`} target="_blank">
+          {postData.title && (
+            <span className={styles.post__heading}>{postData.title}</span>
+          )}
+        </NavLink>
+        {postData.description && (
+          <p className={styles.post__desc}>{postData.description}</p>
+        )}
+      </div>
+    </div>
+  );
 }
