092831ebcee9e409f8df21d3877a1322b366ad95
DarioUrbanVegaIt <dario.urban@vegait.rs>
2023-11-22 09:07:54 +0100

    removed after from migration

--- zuko-api/database/migrations/2023_11_21_121232_add_show_on_website_to_users.php
+++ zuko-api/database/migrations/2023_11_21_121232_add_show_on_website_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('show')->default(false)->after('email_verified_at');
+            $table->boolean('show')->default(false);
         });
     }
 
