d3161bf670c8d990110fee227c8b3a067bbc38f6
Petar Purac <petar.purac@vegait.rs>
2024-01-03 16:54:45 +0100

    Implement changes in styles for website.

--- zuko-ui/src/components/Publications/Publications.jsx
+++ zuko-ui/src/components/Publications/Publications.jsx
@@ -5,11 +5,10 @@ export default function Publications({data}) {
   return (
     <div className={style.publications}>
       {data.map(({cover_image_url, url, title }) => (
-        <a href={url} target="_blank" rel="noreferrer" className={style.card}>
+        <a href={url} target="_blank" rel="noreferrer" className={style.card} key={data.id}>
             <figure className={style.imgContainer}>
                 <img src={cover_image_url} alt={title} className={style.img}/>
             </figure>
-            
             <p className={style.title}>
                 {title}
             </p>
